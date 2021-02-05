43f9a54966aa671a32ad489c7d12964d32798f9f
Michael Amoabeng <michael.amoabeng2015@gmail.com>
2021-02-05 05:53:47 +0100

    Initial Commit

---
+++ TodoManager/TodoManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoManagerApp.swift
+//  TodoManager
+//
+//  Created by Michael Amoabeng on 05.02.21.
+//
+
+import SwiftUI
+
+@main
+struct TodoManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
